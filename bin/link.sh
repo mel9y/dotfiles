dd44fa07c51d2dcf5c3423024b9122de5bc2a865
m2en <me@m2en.dev>
2023-01-29 18:18:15 +0900

    feat: add link script

---
+++ bin/link.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+for dotfile in "${SCRIPT_DIR}"/.??* ; do
+    [[ "$dotfile" == "${SCRIPT_DIR}/.git" ]] && continue
+
+    ln -fnsv "$dotfile" "$HOME"
+done
