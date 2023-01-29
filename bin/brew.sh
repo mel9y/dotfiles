b8e6a4491f1f5ba24a5e4a1be434aae4a6d61085
m2en <me@m2en.dev>
2023-01-29 20:43:23 +0900

    feat: remove docker install part

--- bin/brew.sh
+++ bin/brew.sh
@@ -9,13 +9,10 @@ echo "brew.sh: アプリケーション自動インストールを開始しま�
 brew bundle --global --file '~/dotfiles/bin/brew/Brewfile'
 echo "brew.sh: アプリケーション自動インストールを終了しました"
 
-echo "brew.sh: 別途でインストールが必要なアプリケーションをインストールします"
-echo "  - Docker Desktop"
-brew install --cask docker
-echo "      Docker Desktop のインストールに成功しました"
-
-echo "brew.sh: アプリケーション自動インストールを終了しました"
-echo "brew.sh: 以下のアプリケーションは手動でダウンロードしてください。"
+echo "brew.sh: 以下のアプリケーションは手動でダウンロードしてください"
 echo "  - RunCat"
 echo "  - RunCat Plugins Manager"
 echo "  - BetterSnapTool"
+echo "brew.sh(Info): これらのアプリケーションは AppStore でインストールできます"
+echo "  - Docker / Docker Desktop"
+echo "brew.sh(Info): このアプリケーションは　brew install --cask docker を実行することでインストールできます "
