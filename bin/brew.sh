617b537b926325f7f9f86041780795488e14102b
m2en <me@m2en.dev>
2023-02-01 01:49:10 +0900

    fix: command log message

--- bin/brew.sh
+++ bin/brew.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 if [ "$(uname)" != "Darwin" ] ; then
-	echo "brew.sh(Error): このスクリプトはMacOSでのみ動作します"
+	echo ">> このスクリプトはMacOSでのみ動作します"
 	exit 1
 fi
 
-echo "brew.sh: アプリケーション自動インストールを開始します"
+echo ">> アプリケーション自動インストールを開始します"
 brew bundle --global --file './brew/.Brewfile'
-qecho "brew.sh: アプリケーション自動インストールを終了しました"
+qecho ">> アプリケーション自動インストールを終了しました"
