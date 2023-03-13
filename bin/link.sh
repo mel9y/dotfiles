a7c6c769df3fdabef79b8f3a286a04300057bcd6
mel9y <me@mel9y.dev>
2023-03-13 17:01:48 +0900

    chore: delete old link.sh

--- bin/link.sh
+++ bin/link.sh
@@ -1,32 +1,3 @@
 #!/bin/bash
+# git がカスすぎるのでこれをインストーラーにします。
 
-# https://qiita.com/yutkat/items/c6c7584d9795799ee164
-
-link_to_homedir() {
-  command echo "dotfiles のバックアップを作成します."
-  if [ ! -d "$HOME/.dotbackup" ];then
-    command echo "$HOME/.dotbackup が見つかりません。作成します。"
-    command mkdir "$HOME/.dotbackup"
-  fi
-
-  local script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
-  local dotdir=$(dirname ${script_dir})
-  if [[ "$HOME" != "$dotdir" ]];then
-    for f in $dotdir/.??*; do
-      [[ `basename $f` == ".git" ]] && continue
-      if [[ -L "$HOME/`basename $f`" ]];then
-        command rm -f "$HOME/`basename $f`"
-      fi
-      if [[ -e "$HOME/`basename $f`" ]];then
-        command mv "$HOME/`basename $f`" "$HOME/.dotbackup"
-      fi
-      command ln -snf $f $HOME
-    done
-  else
-    command echo "same install src dest"
-  fi
-}
-
-echo ">> シンボリックリンクの再リンクを開始します"
-link_to_homedir
-echo ">> シンボリックリンクの再リンクに成功しました"
